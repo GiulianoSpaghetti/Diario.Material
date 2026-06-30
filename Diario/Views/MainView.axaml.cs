f09cfdbdd6738aa5a1de0af3e89722d97e397107
Giulio Sorrentino <numerone@fastwebnet.it>
2026-06-30 15:05:44 +0200

    Refactor MainView to use instance ResourceDictionary

--- Diario/Views/MainView.axaml.cs
+++ Diario/Views/MainView.axaml.cs
@@ -1,21 +1,33 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using Avalonia.Threading;
 using Diario;
+using Diario.Pages;
 using Material.Styles.Controls;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Diario.Views;
 
 public partial class MainView : UserControl
 {
     public static MainView Instance;
+    public static ResourceDictionary Dictionary { get; private set; }
+
     public MainView()
     {
+        
         InitializeComponent();
         Instance = this;
-        DrawerList.PointerReleased += DrawerSelectionChanged;
+        Dictionary = this.FindResource(CultureInfo.CurrentCulture.TwoLetterISOLanguageName) as ResourceDictionary;
+        if (Dictionary == null)
+            Dictionary = this.FindResource("it") as ResourceDictionary; MainView.Traduci();
+        HomePage.Traduci();
+        RicercaPage.Traduci();
 
+        DrawerList.PointerReleased += DrawerSelectionChanged;
     }
     public void DrawerSelectionChanged(object? sender, RoutedEventArgs? args)
     {
@@ -44,8 +56,8 @@ public partial class MainView : UserControl
 
     public static void Traduci()
     {
-        Instance.HomeTitle.Content = MainWindow.Dictionary["Home"] as string;
-        Instance.RicercaTitle.Content = MainWindow.Dictionary["RicercaT"] as string;
-        Instance.InfoTitle.Content = MainWindow.Dictionary["Informazioni"] as string;
+        Instance.HomeTitle.Content = Dictionary["Home"] as string;
+        Instance.RicercaTitle.Content = Dictionary["RicercaT"] as string;
+        Instance.InfoTitle.Content = Dictionary["Informazioni"] as string;
     }
 }
