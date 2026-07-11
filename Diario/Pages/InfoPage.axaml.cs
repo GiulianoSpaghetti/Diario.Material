a038ed07c98006b4da9930eae40e19b37a6582eb
Giulio Sorrentino <numerone@fastwebnet.it>
2026-07-11 04:30:18 +0200

    Implement button click to launch GitHub page
    Added a launcher to open the GitHub page when the button is clicked.

--- Diario/Pages/InfoPage.axaml.cs
+++ Diario/Pages/InfoPage.axaml.cs
@@ -11,8 +11,17 @@ namespace Diario.Pages;
 
 public partial class InfoPage : UserControl
 {
+    private static Avalonia.Platform.Storage.ILauncher? launcher=null;
+    public static readonly Uri HomePage=new Uri("https://github.com/GiulianoSpaghetti/Diario.Material");
     public InfoPage()
     {
         InitializeComponent();
+    }
+
+    private void OnSito_Click(object sender, RoutedEventArgs e)
+    {
+        if (launcher==null)
+            launcher = TopLevel.GetTopLevel( (Avalonia.Visual) sender).Launcher;
+        launcher.LaunchUriAsync(HomePage);
     }
 }
